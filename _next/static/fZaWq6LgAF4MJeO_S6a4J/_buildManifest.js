fa442674750ba459932fa3c1396d058e2f60e360
MaGOs92 <MaGOs92@users.noreply.github.com>
2022-03-23 13:36:04 +0000

    deploy: b1f9922828bee0a04acb0dc92ad44bf0d9e12d35

---
+++ _next/static/fZaWq6LgAF4MJeO_S6a4J/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/6c3e69ba36d083c5.css","static/chunks/pages/index-e7c09f31d902bef6.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
