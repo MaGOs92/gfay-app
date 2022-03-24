d8800125ff4c6adc4bcdf5d54e79c9eb6a51005f
MaGOs92 <MaGOs92@users.noreply.github.com>
2022-03-24 17:22:39 +0000

    deploy: 6ac6585e8ac586efa96aaff00399657a24abcaf7

---
+++ _next/static/1TkO1_KyTdKWHb62HVwaO/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/308c85a4442202ba.css","static/chunks/pages/index-1b516b8da1278f53.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/8h1ejPAkJ-eTpokBR8re3/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/308c85a4442202ba.css","static/chunks/pages/index-a26a65cff1e60d41.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
